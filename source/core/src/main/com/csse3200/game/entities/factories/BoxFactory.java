ca1a8d3a1647a3e44ab17c55db4f356e17a0c4f4
Christina Russo <christina.bne@outlook.com>
2025-08-23 08:53:55 +1000

    Static box physics altered to create solid immoveable box

--- source/core/src/main/com/csse3200/game/entities/factories/BoxFactory.java
+++ source/core/src/main/com/csse3200/game/entities/factories/BoxFactory.java
@@ -3,6 +3,7 @@ package com.csse3200.game.entities.factories;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.csse3200.game.entities.Entity;
 import com.csse3200.game.physics.components.ColliderComponent;
 import com.csse3200.game.physics.components.PhysicsComponent;
@@ -12,21 +13,24 @@ public class BoxFactory {
 
     public static Entity createStaticBox() {
 
-        // Placeholder white box rendered via Pixmap
+        // Placeholder grey box rendered via Pixmap
         Pixmap pixmap = new Pixmap(16, 16, Pixmap.Format.RGBA8888);  // 8 bits per RGBA inputs
-        pixmap.setColor(Color.valueOf("FFFFFF"));  // white in hex, converted to LibGDX Color float
+        pixmap.setColor(Color.valueOf("D3D3D3"));  // light grey in hex, converted to LibGDX Color float
         pixmap.fill();
 
         // Convert Pixmap box to Texture
         Texture texture = new Texture(pixmap);  // Replace with new Texture("path/filename.png") for 2d pixel art
         pixmap.dispose();
 
+        // Static physics component
+        PhysicsComponent staticBody = new PhysicsComponent().setBodyType(BodyDef.BodyType.StaticBody);
+
         Entity box = new Entity()
                 .addComponent(new TextureRenderComponent(texture))  // swap out when implementing pixel art
-                .addComponent(new PhysicsComponent())
+                .addComponent(staticBody)
                 .addComponent(new ColliderComponent());
 
-        box.setScale(0.5f, 0.5f);  // 1 x 1 in game world units
+        box.setScale(0.5f, 0.5f);  // 0.5 x 0.5 in game world units
         return box;
     }
 }
