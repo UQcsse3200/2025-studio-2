b441e98b672f3020dda896bca87b83be4da3bfa2
jasmine <jasmineburt7@gmail.com>
2025-08-24 14:24:49 +1000

    Create and initialise ItemConfig.java

---
+++ source/core/src/main/com/csse3200/game/entities/configs/ItemConfig.java
@@ -0,0 +1,9 @@
+package com.csse3200.game.entities.configs;
+
+public class ItemConfig {
+    public String id;
+    public String name;
+    public String type;
+    public String sprite;
+    public String unlockTarget;
+}
