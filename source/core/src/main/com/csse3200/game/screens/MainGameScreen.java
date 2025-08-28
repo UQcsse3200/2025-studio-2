6971bc4a413763a0a81521f47d484570fe607403
s4693599 <lilly.moore@student.uq.edu.au>
2025-08-28 21:10:02 +1000

    Change to create cave game area on start up

--- source/core/src/main/com/csse3200/game/screens/MainGameScreen.java
+++ source/core/src/main/com/csse3200/game/screens/MainGameScreen.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.csse3200.game.GdxGame;
+import com.csse3200.game.areas.CaveGameArea;
 import com.csse3200.game.areas.ForestGameArea;
 import com.csse3200.game.areas.terrain.TerrainFactory;
 import com.csse3200.game.components.maingame.MainGameActions;
@@ -66,8 +67,9 @@ public class MainGameScreen extends ScreenAdapter {
 
     logger.debug("Initialising main game screen entities");
     TerrainFactory terrainFactory = new TerrainFactory(renderer.getCamera());
-    ForestGameArea forestGameArea = new ForestGameArea(terrainFactory);
-    forestGameArea.create();
+    //ForestGameArea forestGameArea = new ForestGameArea(terrainFactory);
+    CaveGameArea caveGameArea = new CaveGameArea(terrainFactory);
+    caveGameArea.create();
   }
 
   @Override
