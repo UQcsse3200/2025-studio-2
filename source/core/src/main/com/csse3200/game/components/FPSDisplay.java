4fca5caf6f4a221ed93c89143c81f9b80cd9c5ad
Matthew Collins <probably0k@proton.me>
2025-08-31 21:40:34 +1000

    Added JavaDocs to FPSDisplay

--- source/core/src/main/com/csse3200/game/components/FPSDisplay.java
+++ source/core/src/main/com/csse3200/game/components/FPSDisplay.java
@@ -6,20 +6,33 @@ import com.csse3200.game.ui.UIComponent;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 
 public class FPSDisplay extends UIComponent {
+    /**
+     * Label used to show the game's current FPS
+     */
     private Label fpsLabel;
 
+    /**
+     * Creates all widgets to be drawn.
+     */
     @Override
     public void create() {
         super.create();
         addActors();
     }
 
+    /**
+     * Initialises the FPS label and adds it to the stage.
+     */
     private void addActors() {
         // Create label with current FPS
         fpsLabel = new Label("FPS: " + Gdx.graphics.getFramesPerSecond(), skin);
         stage.addActor(fpsLabel);
     }
 
+    /**
+     * Draws the FPS label in the top left of the screen.
+     * @param batch Batch to render to.
+     */
     @Override
     public void draw(SpriteBatch batch) {
         // Draw FPS at top-left corner of screen
@@ -29,12 +42,18 @@ public class FPSDisplay extends UIComponent {
         fpsLabel.setPosition(offsetX, screenHeight - offsetY);
     }
 
+    /**
+     * Removes the FPS label.
+     */
     public void dispose() {
         fpsLabel.remove();
         super.dispose();
     }
 
-    public void update() {
+  /**
+   * Resets the text inside the FPS label to be current FPS.
+   */
+  public void update() {
         // Continuously update FPS to contain current FPS
         fpsLabel.setText("FPS: " + Gdx.graphics.getFramesPerSecond());
     }
