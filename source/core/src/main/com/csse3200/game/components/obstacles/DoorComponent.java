4e9ee51cef6f80c573d24a35f6406bd20801b1fc
s4693599 <lilly.moore@student.uq.edu.au>
2025-09-02 10:37:15 +1000

    Add door event trigger and change of image

--- source/core/src/main/com/csse3200/game/components/obstacles/DoorComponent.java
+++ source/core/src/main/com/csse3200/game/components/obstacles/DoorComponent.java
@@ -5,10 +5,13 @@ import com.csse3200.game.components.player.InventoryComponent;
 import com.csse3200.game.entities.Entity;
 import com.csse3200.game.physics.PhysicsLayer;
 import com.csse3200.game.physics.components.ColliderComponent;
+import com.csse3200.game.areas.GameArea;
+import com.csse3200.game.rendering.TextureRenderComponent;
 
 public class DoorComponent extends Component {
     private final String keyId;
     private boolean locked = true;
+    private final GameArea area;
 
     /**
      * A component that represents a door which can be locked or unlocked with a specific key.
@@ -16,8 +19,9 @@ public class DoorComponent extends Component {
      * <p>The door listens for collisions with player entities and checks their inventory
      * for a matching key. If the player has the correct key, the door is unlocked and opened.</p>
      */
-    public DoorComponent(String keyId) {
+    public DoorComponent(String keyId, GameArea area) {
         this.keyId = keyId;
+        this.area = area;
     }
 
 
@@ -29,6 +33,8 @@ public class DoorComponent extends Component {
         entity.getEvents().addListener("collisionStart", this::onCollisionStart);
         entity.getEvents().addListener("openDoor", this::openDoor);
         entity.getEvents().addListener("closeDoor", this::closeDoor);
+
+        this.openDoor();
     }
 
     /**
@@ -44,7 +50,12 @@ public class DoorComponent extends Component {
         ColliderComponent cc = player.getComponent(ColliderComponent.class);
         if (cc == null || cc.getLayer() != PhysicsLayer.PLAYER) return;
 
-        if (locked) tryUnlock(player);
+        if (locked) {
+            tryUnlock(player);
+        } else {
+            // Door already open -> trigger transition
+            this.area.trigger("doorEntered", keyId);
+        }
     }
 
     /**
@@ -60,15 +71,24 @@ public class DoorComponent extends Component {
             inv.useItem(keyId);
             locked = false;
             entity.getEvents().trigger("openDoor");
+
+            // Notify the area
+            this.area.trigger("doorEntered", keyId);
         }
     }
 
     /**
      * Opens the door by making its collider a sensor (non-blocking).
      */
-    private void openDoor() {
+    public void openDoor() {
         ColliderComponent col = entity.getComponent(ColliderComponent.class);
         col.setSensor(true);
+
+        String texture = "images/Gate_open.png";
+        TextureRenderComponent render = entity.getComponent(TextureRenderComponent.class);
+        if (render != null) {
+            render.setTexture(texture);
+        }
     }
 
     /**
@@ -77,6 +97,12 @@ public class DoorComponent extends Component {
     private void closeDoor() {
         ColliderComponent col = entity.getComponent(ColliderComponent.class);
         if (col != null) col.setSensor(false);
+
+        String texture = "images/gate.png";
+        TextureRenderComponent render = entity.getComponent(TextureRenderComponent.class);
+        if (render != null) {
+            render.setTexture(texture);
+        }
     }
 
     /**
