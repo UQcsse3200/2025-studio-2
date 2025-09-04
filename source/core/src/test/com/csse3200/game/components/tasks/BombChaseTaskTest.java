82cbd373054846d275d66c70b76f0f08b41c2cbc
Nathan Nunes <nathanvnunes@gmail.com>
2025-09-04 15:50:02 +1000

    Fixed BombChaseTaskTest

--- source/core/src/test/com/csse3200/game/components/tasks/BombChaseTaskTest.java
+++ source/core/src/test/com/csse3200/game/components/tasks/BombChaseTaskTest.java
@@ -52,7 +52,7 @@ public class BombChaseTaskTest {
 
         Entity entity = makePhysicsEntity().addComponent(ai);
         entity.create();
-        entity.setPosition(0f, 0f);
+        entity.setPosition(1f, 0f);
 
         float initialDistance = entity.getPosition()
                 .dst(target.getPosition().cpy().add(0f, hoverHeight));
