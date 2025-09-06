7b2ae29897a3420cdb23e938c72a27703bcbbd01
charlottemaherr <charlotte.maher@student.uq.edu.au>
2025-09-06 20:09:35 +1000

    Update tests for new ChaseTask that does not use proximity checks (active/inactive changes priority -> set in EnemyFactory based on PlayerDetected/PlayerLost events)

--- source/core/src/main/com/csse3200/game/components/tasks/ChaseTask.java
+++ source/core/src/main/com/csse3200/game/components/tasks/ChaseTask.java
@@ -1,22 +1,12 @@
 package com.csse3200.game.components.tasks;
 
-import com.badlogic.gdx.math.Vector2;
 import com.csse3200.game.ai.tasks.DefaultTask;
 import com.csse3200.game.ai.tasks.PriorityTask;
 import com.csse3200.game.entities.Entity;
-import com.csse3200.game.physics.PhysicsEngine;
-import com.csse3200.game.physics.PhysicsLayer;
-import com.csse3200.game.physics.raycast.RaycastHit;
-import com.csse3200.game.rendering.DebugRenderer;
-import com.csse3200.game.services.ServiceLocator;
 
-/** Chases a target entity until they get too far away or line of sight is lost */
+/** Chases a target entity */
 public class ChaseTask extends DefaultTask implements PriorityTask {
     private final Entity target;
-
-    private final PhysicsEngine physics;
-    private final DebugRenderer debugRenderer;
-    private final RaycastHit hit = new RaycastHit();
     private MovementTask movementTask;
     private boolean active = false;
 
@@ -25,24 +15,28 @@ public class ChaseTask extends DefaultTask implements PriorityTask {
      */
     public ChaseTask(Entity target) {
         this.target = target;
-        physics = ServiceLocator.getPhysicsService().getPhysics();
-        debugRenderer = ServiceLocator.getRenderService().getDebug();
     }
 
+    /**
+     * Allows us to externally activate chase task (i.e. playerDetected)
+     */
     public void activate() {
         if (active) return;
         active = true;
     }
 
+    /**
+     * Allows us to externally deactivate chase task (i.e. playerLost)
+     */
     public void deactivate() {
         if (!active) return;
         active = false;
-        owner.getEntity().getEvents().trigger("chaseEnd");
     }
 
     @Override
     public void start() {
         super.start();
+        if (!active) return;
 
         if (movementTask == null) {
             movementTask = new MovementTask(target.getPosition());
@@ -50,7 +44,6 @@ public class ChaseTask extends DefaultTask implements PriorityTask {
         } else {
             movementTask.setTarget(target.getPosition());
         }
-
         movementTask.start();
 
         this.owner.getEntity().getEvents().trigger("chaseStart");
@@ -58,6 +51,8 @@ public class ChaseTask extends DefaultTask implements PriorityTask {
 
     @Override
     public void update() {
+        if (!active || movementTask == null) return;
+
         movementTask.setTarget(target.getPosition());
         movementTask.update();
         if (movementTask.getStatus() != Status.ACTIVE) {
@@ -68,24 +63,14 @@ public class ChaseTask extends DefaultTask implements PriorityTask {
     @Override
     public void stop() {
         if (movementTask != null) movementTask.stop();
+        deactivate();
         super.stop();
+
+        this.owner.getEntity().getEvents().trigger("chaseEnd");
     }
 
     @Override
     public int getPriority() {
         return active ? 10 : -1;
-    }
-
-    private boolean isTargetVisible() {
-        Vector2 from = owner.getEntity().getCenterPosition();
-        Vector2 to = target.getCenterPosition();
-
-        // If there is an obstacle in the path to the player, not visible.
-        if (physics.raycast(from, to, PhysicsLayer.OBSTACLE, hit)) {
-            debugRenderer.drawLine(from, hit.point);
-            return false;
-        }
-        debugRenderer.drawLine(from, to);
-        return true;
     }
 }
