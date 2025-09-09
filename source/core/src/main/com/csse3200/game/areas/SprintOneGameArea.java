fce940de7bcd4162c53d990edb36d3721fb2a0d9
mostameues187 <149714477+mostameues187@users.noreply.github.com>
2025-09-10 00:26:37 +1000

    Commit to spawn walls in the ForestGameArea

--- source/core/src/main/com/csse3200/game/areas/SprintOneGameArea.java
+++ source/core/src/main/com/csse3200/game/areas/SprintOneGameArea.java
@@ -275,6 +275,38 @@ public class SprintOneGameArea extends GameArea {
         spawnEntityAt(longPlatform, longPlatPos, false, false);
 
     }
+
+    private void spawnWalls() {
+        float ts = terrain.getTileSize();
+
+        // Tall wall on the left
+        GridPoint2 wall1Pos = new GridPoint2(0, 2);
+        Entity wall1 = WallFactory.createWall(
+                0f, 0f,
+                1f * ts, 5f * ts,
+                "images/walls.png"
+        );
+        spawnEntityAt(wall1, wall1Pos, false, false);
+
+        // Shorter wall in the middle
+        GridPoint2 wall2Pos = new GridPoint2(8, 2);
+        Entity wall2 = WallFactory.createWall(
+                0f, 0f,
+                1f * ts, 3f * ts,
+                "images/tile.png"
+        );
+        spawnEntityAt(wall2, wall2Pos, false, false);
+
+        // Another tall wall further right
+        GridPoint2 wall3Pos = new GridPoint2(15, 2);
+        Entity wall3 = WallFactory.createWall(
+                0f, 0f,
+                1f * ts, 6f * ts,
+                "images/walls.png"
+        );
+        spawnEntityAt(wall3, wall3Pos, false, false);
+    }
+
     private void spawnBoxes() {
 
         // Static box
