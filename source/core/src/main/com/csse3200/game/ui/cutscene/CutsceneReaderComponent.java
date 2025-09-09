0b4ebd8b6a6670f4e211b0043f6c51737b2acd45
Matthew Collins <probably0k@proton.me>
2025-09-09 18:07:29 +1000

    Added structures for managing text boxes and backgrounds

--- source/core/src/main/com/csse3200/game/ui/cutscene/CutsceneReaderComponent.java
+++ source/core/src/main/com/csse3200/game/ui/cutscene/CutsceneReaderComponent.java
@@ -5,6 +5,11 @@ import com.csse3200.game.components.Component;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Component that can be attached to an entity representing a cutscene. Reads a script file for a
  * cutscene and generates relevant structures for displaying the cutscene.
@@ -18,6 +23,15 @@ public class CutsceneReaderComponent extends Component {
      * The file path to the cutscene script from relative to the resources root
      */
     private final String scriptPath;
+    /**
+     * Ordered list of all text boxes found in script file. Text boxes contain original formatting
+     */
+    private final List<String> textBoxes = new ArrayList<>();
+    /**
+     * Mapping for text box indexes to desired background's file path. If a mapping does not
+     * exist, current background is re-used for index
+     */
+    private final Map<Integer, String> backgroundMap = new HashMap<>();
 
     public CutsceneReaderComponent(String scriptPath) {
         this.scriptPath = scriptPath;
