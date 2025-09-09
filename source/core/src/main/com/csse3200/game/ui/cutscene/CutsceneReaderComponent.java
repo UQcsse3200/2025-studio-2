0ba7e6cd28b1a025aef479d7dd5b9acc73ea1c90
Matthew Collins <probably0k@proton.me>
2025-09-09 19:33:27 +1000

    CutsceneReaderComponent can now parse script file to create a list of text box information

--- source/core/src/main/com/csse3200/game/ui/cutscene/CutsceneReaderComponent.java
+++ source/core/src/main/com/csse3200/game/ui/cutscene/CutsceneReaderComponent.java
@@ -5,10 +5,9 @@ import com.csse3200.game.components.Component;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Component that can be attached to an entity representing a cutscene. Reads a script file for a
@@ -26,21 +25,83 @@ public class CutsceneReaderComponent extends Component {
     /**
      * Ordered list of all text boxes found in script file. Text boxes contain original formatting
      */
-    private final List<String> textBoxes = new ArrayList<>();
-    /**
-     * Mapping for text box indexes to desired background's file path. If a mapping does not
-     * exist, current background is re-used for index
-     */
-    private final Map<Integer, String> backgroundMap = new HashMap<>();
+    private final List<TextBox> textBoxes = new ArrayList<>();
 
+    /**
+     * Constructs the component with a specified path to the script file it shall read.
+     *
+     * @param scriptPath The path to the script file being read
+     */
     public CutsceneReaderComponent(String scriptPath) {
         this.scriptPath = scriptPath;
     }
 
     @Override
-    public void create() {
+    public void create() throws IOException {
         // Read contents of the script file into a string
         String scriptContents = Gdx.files.internal(scriptPath).readString();
         logger.debug("Script Contents:\n{}", scriptContents);
+        parseScript(scriptContents);
+    }
+
+    /**
+     * Helper method for iterating through the contents of a script file (as a string) and creating
+     * a list of text boxes containing information about background being set.
+     * Text boxes in the list are in the same order as they are parsed.
+     *
+     * @param scriptContents The contents of the script file as a string.
+     * @throws IOException If the script file is empty, or the first line does not set a background.
+     */
+    private void parseScript(String scriptContents) throws IOException {
+        // Split the scriptContents into lines (works on windows and unix systems)
+        String[] scriptLines = scriptContents.split(System.lineSeparator());
+
+        // Throw error if file is empty
+        if (scriptLines.length == 0) {
+            throw new IOException("Script file '" + scriptPath + "' is empty.");
+        }
+
+        int lineIndex = 0;
+        String lastCommand = null;
+        for (String line : scriptLines) {
+            // Check if we need to interpret line as a text box or command
+            boolean isText = line.isEmpty() || line.charAt(0) != '#';
+
+            // Throw error if first line in file does not set a background
+            if (lineIndex == 0 && isText) {
+                throw new IOException("First line in script file '" + scriptPath + "' does not " +
+                        "set background.");
+            }
+
+            // Parse line as text box or command
+            if (isText) {
+                textBoxes.add(new TextBox(line, lastCommand));
+                lastCommand = null;
+            } else {
+                lastCommand = line;
+            }
+
+            lineIndex++;
+        }
+
+    }
+
+    /**
+     * Getter method that returns the parsed text boxes from the parsed script file.
+     *
+     * @return A list containing text boxes in the order they were parsed.
+     */
+    public List<TextBox> getTextBoxes() {
+        return textBoxes;
+    }
+
+    /**
+     * Record object that represents information about a text box.
+     *
+     * @param text       The text to be drawn inside the text box.
+     * @param background null means use the current background. Otherwise, can contain path to
+     *                   background asset to be used for this text box.
+     */
+    public record TextBox(String text, String background) {
     }
 }
