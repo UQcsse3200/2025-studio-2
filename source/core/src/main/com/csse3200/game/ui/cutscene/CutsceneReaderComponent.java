8af7794dab36046118c786d65cc4ae2b43e76674
Matthew Collins <probably0k@proton.me>
2025-09-09 17:08:15 +1000

    Created component for reading script file for cutscene

---
+++ source/core/src/main/com/csse3200/game/ui/cutscene/CutsceneReaderComponent.java
@@ -0,0 +1,10 @@
+package com.csse3200.game.ui.cutscene;
+
+import com.csse3200.game.entities.Entity;
+
+/**
+ * Component that can be attached to an entity representing a cutscene. Reads a script file for a
+ * cutscene and generates relevant structures for displaying the cutscene.
+ */
+public class CutsceneReaderComponent extends Entity {
+}
