f0c78ef2e7e3dc7bd8365786d61a15071490260d
Matthew Collins <probably0k@proton.me>
2025-09-09 19:59:13 +1000

    Created methods for mocking LibGdx file read and setting up each test

--- source/core/src/test/com/csse3200/game/ui/cutscene/CutsceneReaderComponentTest.java
+++ source/core/src/test/com/csse3200/game/ui/cutscene/CutsceneReaderComponentTest.java
@@ -1,4 +1,52 @@
 package com.csse3200.game.ui.cutscene;
 
+import com.badlogic.gdx.Files;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.csse3200.game.extensions.GameExtension;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith(GameExtension.class)
 public class CutsceneReaderComponentTest {
+    // Dummy path to cutscene script
+    private static final String DUMMY_PATH = "cutscene-scripts/test.txt";
+    // Component instance being tested
+    CutsceneReaderComponent cutsceneReader;
+    // Mocks for LibGDX dependencies
+    @Mock
+    private Files mockFiles;
+    @Mock
+    private FileHandle mockFileHandle;
+
+    @BeforeEach
+    void setUp() {
+        // Mock static Gdx.files field
+        Gdx.files = mockFiles;
+        // Make internal() return mock file handle
+        when(mockFiles.internal(DUMMY_PATH)).thenReturn(mockFileHandle);
+    }
+
+    @AfterEach
+    void reset() {
+        // Revert static Gdx.files field
+        Gdx.files = null;
+    }
+
+    /**
+     * Mocks the content that is read from a script file.
+     *
+     * @param content A string containing contents of read file.
+     */
+    private void mockScriptContent(String content) {
+        when(mockFileHandle.readString()).thenReturn(content);
+    }
 }
