ea2dafd792b3224e52367cbc46a83939a37795ec
Matthew Collins <probably0k@proton.me>
2025-09-09 17:37:12 +1000

    CutsceneReaderComponent is able to read contents of a file

--- source/core/src/main/com/csse3200/game/ui/cutscene/CutsceneReaderComponent.java
+++ source/core/src/main/com/csse3200/game/ui/cutscene/CutsceneReaderComponent.java
@@ -1,10 +1,25 @@
 package com.csse3200.game.ui.cutscene;
 
-import com.csse3200.game.entities.Entity;
+import com.badlogic.gdx.Gdx;
+import com.csse3200.game.components.Component;
 
 /**
  * Component that can be attached to an entity representing a cutscene. Reads a script file for a
  * cutscene and generates relevant structures for displaying the cutscene.
  */
-public class CutsceneReaderComponent extends Entity {
+public class CutsceneReaderComponent extends Component {
+    /**
+     * The file path to the cutscene script from relative to the resources root
+     */
+    private final String scriptPath;
+
+    public CutsceneReaderComponent(String scriptPath) {
+        this.scriptPath = scriptPath;
+    }
+
+    @Override
+    public void create() {
+        // Read contents of the script file into a string
+        String scriptContents = Gdx.files.internal(scriptPath).readString();
+    }
 }
