4643a8b148e9ca250538e6fccc1bf6a8119b12ea
Matthew Collins <probably0k@proton.me>
2025-09-11 11:10:44 +1000

    CutsceneDisplay creates a root table and can take text boxes from reader. Disposes nicely

--- source/core/src/main/com/csse3200/game/ui/cutscene/CutsceneDisplay.java
+++ source/core/src/main/com/csse3200/game/ui/cutscene/CutsceneDisplay.java
@@ -1,11 +1,51 @@
 package com.csse3200.game.ui.cutscene;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.csse3200.game.ui.UIComponent;
+import com.csse3200.game.ui.cutscene.CutsceneReaderComponent.TextBox;
+
+import java.util.List;
 
 public class CutsceneDisplay extends UIComponent {
+    /**
+     * Ordered list of text boxes to be displayed in cutscene
+     */
+    private final List<TextBox> textBoxList;
+    /**
+     * Root table to be rendered
+     */
+    private Table rootTable;
+
+    /**
+     * Initialises display with text box information
+     * @param textBoxList A list of all text boxes created from a CutsceneRenderComponent
+     */
+    public CutsceneDisplay(List<TextBox> textBoxList) {
+        this.textBoxList = textBoxList;
+    }
+
+    @Override
+    public void create() {
+        super.create();
+
+        // Create root table - fills screen
+        rootTable = new Table();
+        rootTable.setFillParent(true);
+    }
+
+
+
     @Override
     protected void draw(SpriteBatch batch) {
+        // Handled by Scene2D
+    }
 
+    @Override
+    public void dispose() {
+        super.dispose();
+        if (rootTable != null) {
+            rootTable.remove();
+        }
     }
 }
