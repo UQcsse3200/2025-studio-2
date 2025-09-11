3a2167a71fce26e08c01c93abd1d308daace8dd5
Matthew Collins <probably0k@proton.me>
2025-09-11 11:00:00 +1000

    Created CutsceneDisplay component - attaches to a cutscene entity and can be used to display textboxes/background

---
+++ source/core/src/main/com/csse3200/game/ui/cutscene/CutsceneDisplay.java
@@ -0,0 +1,11 @@
+package com.csse3200.game.ui.cutscene;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.csse3200.game.ui.UIComponent;
+
+public class CutsceneDisplay extends UIComponent {
+    @Override
+    protected void draw(SpriteBatch batch) {
+
+    }
+}
