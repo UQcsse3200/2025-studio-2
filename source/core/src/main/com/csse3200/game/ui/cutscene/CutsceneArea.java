8ea7be03c1789a9e0d52a4956941e561231d7b1c
Matthew Collins <probably0k@proton.me>
2025-09-12 15:53:13 +1000

    Modified CutsceneArea to fit with new GameArea scheme (WIP)

--- source/core/src/main/com/csse3200/game/ui/cutscene/CutsceneArea.java
+++ source/core/src/main/com/csse3200/game/ui/cutscene/CutsceneArea.java
@@ -1,12 +1,16 @@
 package com.csse3200.game.ui.cutscene;
 
 import com.csse3200.game.areas.GameArea;
+import com.csse3200.game.components.Component;
 import com.csse3200.game.entities.Entity;
 import com.csse3200.game.entities.factories.PlayerFactory;
 import com.csse3200.game.services.ResourceService;
 import com.csse3200.game.services.ServiceLocator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Special game area that can be used to display a cutscene. Works as a game level, so can be
@@ -17,13 +21,6 @@ public class CutsceneArea extends GameArea {
      * Logger for cutscene area
      */
     private static final Logger logger = LoggerFactory.getLogger(CutsceneArea.class);
-    /**
-     * Dummy assets that need to be loaded in a GameArea
-     */
-    private static final String[] dummyAssets = {
-            "images/box_boy_leaf.png",
-            "images/minimap_player_marker.png"
-    };
     /**
      * Cutscene script path
      */
@@ -36,10 +33,6 @@ public class CutsceneArea extends GameArea {
      * Reader entity that can read a script file
      */
     private Entity reader;
-    /**
-     * Cutscene backgrounds
-     */
-    private String[] backgrounds;
 
     /**
      * Constructor for creating the game area representing cutscene.
@@ -52,44 +45,58 @@ public class CutsceneArea extends GameArea {
     }
 
     /**
+     * There are no pre-reqs for cutscene areas
+     */
+    @Override
+    protected void loadPrerequisites() {
+        // Does nothing for cutscene
+    }
+
+    /**
      * Creates a reader entity with parses a script file, then an entity for displaying/handling UI.
      * Additionally, this method creates a dummy player (requirement of GameArea)
      */
     @Override
-    public void create() {
+    protected void loadEntities() {
         // Create reader and read
         reader = new Entity();
         reader.addComponent(new CutsceneReaderComponent(scriptPath));
         spawnEntity(reader);
 
-        // Load background assets from script
-        loadAssets();
+        // Load backgrounds from the reader
+        loadBackgroundsFromReader();
 
         // Create cutscene UI
         Entity cutscene = new Entity();
         cutscene.addComponent(new CutsceneDisplay(reader.getComponent(CutsceneReaderComponent.class).getTextBoxes(), this, nextAreaID));
         spawnEntity(cutscene);
-
-        // Create dummy player
-        player = PlayerFactory.createPlayer();
     }
 
     /**
      * Private method for loading all assets into the resource service. This includes dummy assets
      * for the minimap and player, as well as backgrounds for the cutscene
      */
-    private void loadAssets() {
-        logger.debug("Attempting to load background assets");
+    protected void loadAssets() {
+        logger.debug("Loading assets");
+        // Get resource service
+        ResourceService resourceService = ServiceLocator.getResourceService();
 
+        // Do nothing for now, might need to load some dummy assets here
+
+        // Show loading progress in logs
+        while (!resourceService.loadForMillis(10)) {
+            logger.info("Loading... {}%", resourceService.getProgress());
+        }
+    }
+
+    private void loadBackgroundsFromReader() {
+        logger.debug("Loading background assets for cutscene");
         // Get resource service
         ResourceService resourceService = ServiceLocator.getResourceService();
 
         // Get reader component, and send background asset paths to resource service
         CutsceneReaderComponent readerComp = reader.getComponent(CutsceneReaderComponent.class);
         resourceService.loadTextures(readerComp.getBackgrounds());
-
-        // Need to load some dummy assets to prevent crashing
-        resourceService.loadTextures(dummyAssets);
 
         // Show loading progress in logs
         while (!resourceService.loadForMillis(10)) {
@@ -110,17 +117,36 @@ public class CutsceneArea extends GameArea {
         CutsceneReaderComponent readerComp = reader.getComponent(CutsceneReaderComponent.class);
         resourceService.unloadAssets(readerComp.getBackgrounds());
 
-        // Unload dummy assets
-        resourceService.unloadAssets(dummyAssets);
-
+        // May also need to unload dummy assets if added
     }
 
     /**
-     * Unused - area is never reset (requirement of GameArea)
+     * Cutscene areas cannot be reset
      */
     @Override
     protected void reset() {
         // Reset does nothing for cutscene
+    }
+
+
+    /**
+     * Create dummy player for cutscene
+     */
+    @Override
+    protected Entity spawnPlayer() {
+        Entity dummyPlayer = PlayerFactory.createPlayer(new ArrayList<>());
+        dummyPlayer.getEvents().addListener("reset", this::reset);
+        return dummyPlayer;
+    }
+
+    /**
+     * Create dummy player for cutscene
+     */
+    @Override
+    protected Entity spawnPlayer(List<Component> componentList) {
+        Entity dummyPlayer = PlayerFactory.createPlayer(componentList);
+        dummyPlayer.getEvents().addListener("reset", this::reset);
+        return dummyPlayer;
     }
 
     /**
@@ -128,10 +154,7 @@ public class CutsceneArea extends GameArea {
      */
     @Override
     public void dispose() {
-        // Remove dummy player
-        ServiceLocator.getEntityService().unregister(player);
-
-        unloadAssets();
         super.dispose();
+        unloadAssets();
     }
 }
