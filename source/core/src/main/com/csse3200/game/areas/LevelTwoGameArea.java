f452414f7a8a2275bb31dad6b359bba326924d77
49745962 <sanyamsingh.brar@student.uq.edu.au>
2025-09-16 17:33:00 +1000

    fix: crashes due to renaming of Empty.png to empty.png

--- source/core/src/main/com/csse3200/game/areas/LevelTwoGameArea.java
+++ source/core/src/main/com/csse3200/game/areas/LevelTwoGameArea.java
@@ -52,7 +52,7 @@ public class LevelTwoGameArea extends GameArea {
             "images/TechWallVariant2.png",
             "images/TechWallVariant3.png",
             "images/platform.png",
-            "images/Empty.png",
+            "images/empty.png",
             "images/gate.png",
             "images/door_open.png",
             "images/door_closed.png",
