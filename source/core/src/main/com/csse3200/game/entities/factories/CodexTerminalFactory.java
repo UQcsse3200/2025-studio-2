3c78d7dff72f51fa9b300dc8f0820f35de143f22
Matthew Collins <probably0k@proton.me>
2025-09-24 22:45:12 +1000

    Created skeleton CodexTerminalFactory class.

---
+++ source/core/src/main/com/csse3200/game/entities/factories/CodexTerminalFactory.java
@@ -0,0 +1,10 @@
+package com.csse3200.game.entities.factories;
+
+/**
+ * Factory class for spawning terminals used to unlock codex entries.
+ */
+public class CodexTerminalFactory {
+  private CodexTerminalFactory() {
+    throw new IllegalStateException("Instantiating static util class");
+  }
+}
