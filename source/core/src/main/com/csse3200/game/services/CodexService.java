0eca96fd9600d1f2d22e2e202a5a9cb8e57f72a1
Matthew Collins <probably0k@proton.me>
2025-09-26 18:58:44 +1000

    Added ability to load entries into the codex entry map in CodexService via a file

--- source/core/src/main/com/csse3200/game/services/CodexService.java
+++ source/core/src/main/com/csse3200/game/services/CodexService.java
@@ -1,5 +1,6 @@
 package com.csse3200.game.services;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.utils.Disposable;
 
 import java.util.HashMap;
@@ -10,16 +11,50 @@ import java.util.Map;
  */
 public class CodexService implements Disposable {
     /**
-     * Map that maps an entry ID to an entry record
+     * Map that maps an entry title to an entry content's (text, unlock status)
      */
     private final Map<String, CodexEntry> entries;
 
+    /**
+     * Constructor for the codex service. Initialises a hash map for all entries
+     */
     public CodexService() {
         entries = new HashMap<>();
     }
 
-    public loadEntries
+    /**
+     * Reads the contents of a file and interprets it as title/text for entries. Even numbered lines
+     * are the title of the entry, subsequent line is considered text description.
+     * @param filePath The path to the file to read, with respect to the resources root.
+     */
+    public void loadEntries(String filePath) {
+        // Read the contents of the file
+        String fileContents = Gdx.files.internal(filePath).readString();
+        String[] fileLines = fileContents.split("\\r?\\n");
 
+        // Keep track of line index, and last recorded title in file
+        int lineIndex = 0;
+        String titleBuffer = null;
+
+        // Iterate through file line by line
+        for (String line : fileLines) {
+            if (lineIndex % 2 == 0) {
+                // Even line numbers are titles
+                titleBuffer = line;
+            } else {
+                // Odd line numbers are text
+                // Enough information for entry - add to codex map
+                entries.put(titleBuffer, new CodexEntry(line));
+                titleBuffer = null;
+            }
+
+            lineIndex++;
+        }
+    }
+
+    /**
+     * Clears the map of
+     */
     @Override
     public void dispose() {
         entries.clear();
