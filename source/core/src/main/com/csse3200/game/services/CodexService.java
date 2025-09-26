bad97511984c3054f0b43b5f0540d4945b1e752f
Matthew Collins <probably0k@proton.me>
2025-09-26 18:30:40 +1000

    CodexEntry is now a regular class. The reasoning for this is that the unlocked flag is mutable.

--- source/core/src/main/com/csse3200/game/services/CodexService.java
+++ source/core/src/main/com/csse3200/game/services/CodexService.java
@@ -2,12 +2,27 @@ package com.csse3200.game.services;
 
 import com.badlogic.gdx.utils.Disposable;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Service for managing all codex entries in the game
  */
 public class CodexService implements Disposable {
+    /**
+     * Map that maps an entry ID to an entry record
+     */
+    private final Map<String, CodexEntry> entries;
+
+    public CodexService() {
+        entries = new HashMap<>();
+    }
+
+    public loadEntries
+
     @Override
     public void dispose() {
-
+        entries.clear();
     }
 }
+
