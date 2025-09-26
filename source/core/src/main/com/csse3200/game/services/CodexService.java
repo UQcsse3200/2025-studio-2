1a2d7e8109601fc685d4015391cf61f25ee8602b
Matthew Collins <probably0k@proton.me>
2025-09-26 16:28:30 +1000

    Created skeleton code for CodexService and added it to the ServiceLocator

---
+++ source/core/src/main/com/csse3200/game/services/CodexService.java
@@ -0,0 +1,13 @@
+package com.csse3200.game.services;
+
+import com.badlogic.gdx.utils.Disposable;
+
+/**
+ * Service for managing all codex entries in the game
+ */
+public class CodexService implements Disposable {
+    @Override
+    public void dispose() {
+
+    }
+}
