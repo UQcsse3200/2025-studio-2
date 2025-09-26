4c328713e3d794e16eeae9d0879fbc677d055887
Matthew Collins <probably0k@proton.me>
2025-09-26 18:35:12 +1000

    CodexEntry is now a regular class. The reasoning for this is that the unlocked flag is mutable.

--- source/core/src/main/com/csse3200/game/services/CodexEntry.java
+++ source/core/src/main/com/csse3200/game/services/CodexEntry.java
@@ -3,5 +3,44 @@ package com.csse3200.game.services;
 /**
  * Class representing a codex entry - storing all relevant data for an entry.
  */
-public record CodexEntry(String title, String description) {
+public class CodexEntry {
+    /**
+     * Text contents of codex entry.
+     */
+    private final String text;
+    /**
+     * Flag determining if codex entry has been unlocked.
+     */
+    private boolean unlocked = false;
+
+    /**
+     * Constructor that sets the text contents of the codex.
+     * @param text A string (with formatting) for the codex entry.
+     */
+    public CodexEntry(String text) {
+        this.text = text;
+    }
+
+    /**
+     * Set the unlocked flag to be true - use when player has unlocked entry;
+     */
+    public void setUnlocked() {
+        unlocked = true;
+    }
+
+    /**
+     * Get the state of the flag determining unlocked status.
+     * @return The flag determining unlocked status.
+     */
+    public boolean isUnlocked() {
+        return unlocked;
+    }
+
+    /**
+     * Get the text contents of the entry.
+     * @return The text contents of the entry.
+     */
+    public String getText() {
+        return text;
+    }
 }
