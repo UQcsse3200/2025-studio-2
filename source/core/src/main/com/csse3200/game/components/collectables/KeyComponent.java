d91514aab53d23105e753d8d5826c0455a6da483
s4693599 <lilly.moore@student.uq.edu.au>
2025-09-26 11:16:28 +1000

    Add sound to key entity when picked up

--- source/core/src/main/com/csse3200/game/components/collectables/KeyComponent.java
+++ source/core/src/main/com/csse3200/game/components/collectables/KeyComponent.java
@@ -1,7 +1,9 @@
 package com.csse3200.game.components.collectables;
 
+import com.badlogic.gdx.audio.Sound;
 import com.csse3200.game.components.player.InventoryComponent;
 import com.csse3200.game.entities.Entity;
+import com.csse3200.game.services.ServiceLocator;
 
 /**
  * A collectable key component that allows the player to pick up and store keys
@@ -39,6 +41,10 @@ public class KeyComponent extends CollectableComponent {
     @Override
     protected boolean onCollect(Entity player) {
         if (player == null) return false;
+
+        Sound interactSound = ServiceLocator.getResourceService().getAsset(
+                "sounds/chimesound.mp3", Sound.class);
+        interactSound.play();
 
         var inventory = player.getComponent(InventoryComponent.class);
         if (inventory != null) {
