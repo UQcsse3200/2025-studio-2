711639f13cd24da14efff87699e3695ae2bf6019
Matthew Collins <probably0k@proton.me>
2025-09-26 18:09:52 +1000

    Added the CodexEntry record. Stores data from an entry

---
+++ source/core/src/main/com/csse3200/game/services/CodexEntry.java
@@ -0,0 +1,7 @@
+package com.csse3200.game.services;
+
+/**
+ * Class representing a codex entry - storing all relevant data for an entry.
+ */
+public record CodexEntry(String title, String description) {
+}
