0c83108762959727c4b5b0ea2c4e971a2bff7b62
Matthew Collins <probably0k@proton.me>
2025-09-30 15:44:56 +1000

    CodexService loads entries on construction without explicit call to CodexService.loadEntries()

--- source/core/src/main/com/csse3200/game/services/CodexService.java
+++ source/core/src/main/com/csse3200/game/services/CodexService.java
@@ -20,7 +20,15 @@ public class CodexService implements Disposable {
     private final Map<String, CodexEntry> entries = new LinkedHashMap<>();
 
     /**
+     * Constructor loads all codex entries from a special file.
+     */
+    public CodexService() {
+        loadEntries();
+    }
+
+    /**
      * Returns an entry held by the service using the key (or id) of the entry.
+     *
      * @param id The id of the entry.
      * @return All entries.
      */
@@ -37,6 +45,7 @@ public class CodexService implements Disposable {
 
     /**
      * Returns all unlocked entries currently stored by the service as an array list.
+     *
      * @return All unlocked entries stored by service as an array list.
      */
     public ArrayList<CodexEntry> getEntries() {
@@ -48,13 +57,12 @@ public class CodexService implements Disposable {
     }
 
     /**
-     * Reads the contents of a file and interprets it as ID, title, and text for entries.
-     *
-     * @param filePath The path to the file to read, with respect to the resources root.
+     * Reads the contents of 'codex-entries.txt' and interprets it as ID, title, and text for
+     * entries.
      */
-    public void loadEntries(String filePath) {
+    private void loadEntries() {
         // Read the contents of the file
-        String fileContents = Gdx.files.internal(filePath).readString();
+        String fileContents = Gdx.files.internal("codex-entries.txt").readString();
         String[] fileLines = fileContents.split("\\r?\\n");
 
         // Iterate through file by three lines each
