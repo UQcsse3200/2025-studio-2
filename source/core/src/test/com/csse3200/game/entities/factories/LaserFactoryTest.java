a8fdb23250c0bc80ff33f79b011cc0a5290e7d0d
Tristyn Hunt <tristyn.hunt@student.uq.edu.au>
2025-10-01 16:08:16 +1000

    #140 #158 added remaining tests for all laser components and factories

---
+++ source/core/src/test/com/csse3200/game/entities/factories/LaserFactoryTest.java
@@ -0,0 +1,60 @@
+package com.csse3200.game.entities.factories;
+
+import box2dLight.RayHandler;
+import com.csse3200.game.components.CombatStatsComponent;
+import com.csse3200.game.components.lasers.LaserEmitterComponent;
+import com.csse3200.game.components.lighting.ConeLightComponent;
+import com.csse3200.game.entities.Entity;
+import com.csse3200.game.lighting.LightingEngine;
+import com.csse3200.game.lighting.LightingService;
+import com.csse3200.game.physics.PhysicsService;
+import com.csse3200.game.rendering.AnimationRenderComponent;
+import com.csse3200.game.rendering.LaserRenderComponent;
+import com.csse3200.game.rendering.RenderService;
+import com.csse3200.game.services.GameTime;
+import com.csse3200.game.services.ResourceService;
+import com.csse3200.game.services.ServiceLocator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class LaserFactoryTest {
+    @BeforeEach
+    void setUp() {
+        // Register PhysicsService to initialise a box's physics body during tests
+        ServiceLocator.registerPhysicsService(new PhysicsService());
+
+        // Mock ResourceService so assets won't throw exceptions
+        ResourceService mockResourceService = mock(ResourceService.class);
+        when(mockResourceService.getAsset(anyString(), any())).thenReturn(null);
+        ServiceLocator.registerResourceService(mockResourceService);
+
+        // mock time source
+        GameTime time = mock(GameTime.class);
+        ServiceLocator.registerTimeSource(time);
+
+        RenderService renderService = new RenderService();
+        ServiceLocator.registerRenderService(renderService);
+
+        RayHandler rayHandler = mock(RayHandler.class);
+        LightingEngine lightingEngine = mock(LightingEngine.class);
+        when(lightingEngine.getRayHandler()).thenReturn(rayHandler);
+        ServiceLocator.registerLightingService(new LightingService(lightingEngine));
+    }
+
+    @Test
+    void createLaserEmitter_hasAllComponents() {
+        Entity e = LaserFactory.createLaserEmitter(0f);
+        assertNotNull(e);
+        assertNotNull(e.getComponent(AnimationRenderComponent.class));
+        assertNotNull(e.getComponent(ConeLightComponent.class));
+        assertNotNull(e.getComponent(CombatStatsComponent.class));
+        assertNotNull(e.getComponent(LaserEmitterComponent.class));
+        assertNotNull(e.getComponent(LaserRenderComponent.class));
+    }
+}
