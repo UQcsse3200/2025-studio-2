d74294bb7221e3425fe215e877640ba1e01f62c8
Alice Walker <alice.walker9@icloud.com>
2025-10-03 18:20:41 +1000

    #140 #179 Added javadoc to SignpostFactory and created SignpostFactoryTest

--- source/core/src/main/com/csse3200/game/entities/factories/SignpostFactory.java
+++ source/core/src/main/com/csse3200/game/entities/factories/SignpostFactory.java
@@ -1,12 +1,14 @@
 package com.csse3200.game.entities.factories;
 
-import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.csse3200.game.entities.Entity;
-import com.csse3200.game.physics.PhysicsLayer;
-import com.csse3200.game.physics.components.ColliderComponent;
-import com.csse3200.game.physics.components.PhysicsComponent;
 import com.csse3200.game.rendering.TextureRenderComponent;
 
+/**
+ * Signpost Factory for creating signpost entities
+ *
+ * Image texture depends on direction for arrow to face (left, right, up or down). If anything
+ *  else specified the image will be a standard signpost with no arrow
+ */
 public class SignpostFactory {
     static final String up = "images/upSignpost.png";
     static final String down = "images/downSignpost.png";
@@ -14,6 +16,17 @@ public class SignpostFactory {
     static final String right = "images/rightSignpost.png";
     static final String standard = "images/signpost.png";
 
+    /**
+     * Creates a signpost entity with the arrow in the specified direction
+     *  - The texture is set to layer 0 so that it is always behind all other entities
+     *  - There is no physics or collider component so nothing can interact with the signpost, and no collisions
+     *  occur with other entities
+     *
+     * @param direction direction for arrow on sign to be (left, right, up, down or
+     *                  standard with no arrow if anything else specified)
+     *
+     * @return fully configured signpost entity
+     */
     public static Entity createSignpost(String direction) {
         Entity signpost = new Entity();
         switch (direction) {
