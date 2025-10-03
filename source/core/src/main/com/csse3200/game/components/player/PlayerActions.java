529e8fdaa170b1f961534bc020b03b0b7c8d645a
s4693599 <lilly.moore@student.uq.edu.au>
2025-10-03 17:09:02 +1000

    Add walk, jetpack and dash sounds

--- source/core/src/main/com/csse3200/game/components/player/PlayerActions.java
+++ source/core/src/main/com/csse3200/game/components/player/PlayerActions.java
@@ -67,7 +67,9 @@ public class PlayerActions extends Component {
   private boolean isGliding = false;
 
   private Sound jetpackSound = ServiceLocator.getResourceService().getAsset(
-          "sounds/Impact4.ogg", Sound.class);
+          "sounds/jetpacksound.mp3", Sound.class);
+  private Sound walkSound = ServiceLocator.getResourceService().getAsset(
+          "sounds/walksound.mp3", Sound.class);
 
   @Override
   public void create() {
@@ -75,6 +77,8 @@ public class PlayerActions extends Component {
     combatStatsComponent = entity.getComponent(CombatStatsComponent.class);
     cameraComponent = entity.getComponent(CameraComponent.class);
     stamina = entity.getComponent(StaminaComponent.class);
+    walkSound.loop();
+    walkSound.pause();
 
     entity.getEvents().addListener("walk", this::walk);
     entity.getEvents().addListener("walkStop", this::stopWalking);
@@ -214,6 +218,7 @@ public class PlayerActions extends Component {
    */
   void walk(Vector2 direction) {
       this.walkDirection.set(direction); // <- keep/make this
+      walkSound.resume();
       moving = true;
   }
 
@@ -232,6 +237,7 @@ public class PlayerActions extends Component {
   void stopWalking() {
     this.walkDirection.setZero();
     updateSpeed(); // apply zero desired velocity so we decelerate immediately
+    walkSound.pause();
     moving = false;
   }
 
@@ -276,7 +282,7 @@ public class PlayerActions extends Component {
 
     Sound interactSound = ServiceLocator.getResourceService().getAsset(
             "sounds/thudsound.mp3", Sound.class);
-    interactSound.play(0.05f);
+    interactSound.play(0.08f);
   }
 
   /**
@@ -307,6 +313,11 @@ public class PlayerActions extends Component {
 
     // Scale the direction vector to increase speed
     this.walkDirection.scl(DASH_SPEED_MULTIPLIER);
+
+    Sound interactSound = ServiceLocator.getResourceService().getAsset(
+            "sounds/whooshsound.mp3", Sound.class);
+    interactSound.play(0.2f);
+
     body.applyLinearImpulse(new Vector2(this.walkDirection.x, 0f), body.getWorldCenter(), true);
     // Unscale the direction vector to ensure player does not infinitely dash in one direction
     this.walkDirection.scl((float) 1 / DASH_SPEED_MULTIPLIER);
