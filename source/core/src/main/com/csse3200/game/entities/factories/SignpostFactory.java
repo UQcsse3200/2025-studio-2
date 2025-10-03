6028c4b1cf1627f7c83b0c015fc0cb9a7d2d35e2
Alice Walker <alice.walker9@icloud.com>
2025-10-03 17:38:49 +1000

    Added createSignpost to SignpostFactory that takes the desired arrow direction and spawns the entity with this arrow direction

--- source/core/src/main/com/csse3200/game/entities/factories/SignpostFactory.java
+++ source/core/src/main/com/csse3200/game/entities/factories/SignpostFactory.java
@@ -1,5 +1,48 @@
 package com.csse3200.game.entities.factories;
 
-public class SignpostFactory {
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.csse3200.game.entities.Entity;
+import com.csse3200.game.physics.PhysicsLayer;
+import com.csse3200.game.physics.components.ColliderComponent;
+import com.csse3200.game.physics.components.PhysicsComponent;
+import com.csse3200.game.rendering.TextureRenderComponent;
 
+public class SignpostFactory {
+    static final String up = "images/upSignpost.png";
+    static final String down = "images/downSignpost.png";
+    static final String left = "images/leftSignpost.png";
+    static final String right = "images/rightSignpost.png";
+    static final String standard = "images/signpost.png";
+
+    public static Entity createSignpost(String direction) {
+        Entity signpost = new Entity();
+        switch (direction) {
+            case "up" -> {
+                TextureRenderComponent textureRenderComponent = new TextureRenderComponent(up);
+                textureRenderComponent.setLayer(0);
+                signpost.addComponent(textureRenderComponent);
+            }
+            case "down" -> {
+                TextureRenderComponent textureRenderComponent = new TextureRenderComponent(down);
+                textureRenderComponent.setLayer(0);
+                signpost.addComponent(textureRenderComponent);
+            }
+            case "left" -> {
+                TextureRenderComponent textureRenderComponent = new TextureRenderComponent(left);
+                textureRenderComponent.setLayer(0);
+                signpost.addComponent(textureRenderComponent);
+            }
+            case "right" -> {
+                TextureRenderComponent textureRenderComponent = new TextureRenderComponent(right);
+                textureRenderComponent.setLayer(0);
+                signpost.addComponent(textureRenderComponent);
+            }
+            default ->  {
+                TextureRenderComponent textureRenderComponent = new TextureRenderComponent(standard);
+                textureRenderComponent.setLayer(0);
+                signpost.addComponent(textureRenderComponent);
+            }
+        }
+        return signpost;
+    }
 }
