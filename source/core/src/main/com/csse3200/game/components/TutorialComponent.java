15354cd637dc4891a9a443ee3196c3a5f7cdc431
49745962 <sanyamsingh.brar@student.uq.edu.au>
2025-10-07 18:18:20 +1000

    updated the implementation to render font properly

--- source/core/src/main/com/csse3200/game/components/TutorialComponent.java
+++ source/core/src/main/com/csse3200/game/components/TutorialComponent.java
@@ -1,11 +1,17 @@
 package com.csse3200.game.components;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.Align;
 import com.csse3200.game.rendering.TextureRenderComponent;
 import com.csse3200.game.services.ServiceLocator;
 import com.csse3200.game.ui.UIComponent;
@@ -13,54 +19,46 @@ import com.csse3200.game.ui.UIComponent;
 /**
  * A UI component that displays a tutorial image and corresponding key as text.
  */
-public class TutorialComponent extends UIComponent {
-
+public class TutorialComponent extends TextureRenderComponent {
   private final String keyText;
-  private Table tutorialTable;
-  private CameraComponent camera;
-  private final Texture actionTexture;
-
+  private BitmapFont font;
+  Skin skin;
 
   /**
    * @param imagePath Path to the texture for the action's image.
    * @param keyText The text to display for the key name
    */
   public TutorialComponent(String imagePath, String keyText) {
-    this.actionTexture = ServiceLocator.getResourceService().getAsset(imagePath, Texture.class);
+    super(imagePath);
     this.keyText = keyText;
   }
 
   @Override
   public void create() {
     super.create();
-    camera = ServiceLocator.getRenderService().getRenderer().getCamera();
-
-    Image actionImage = new Image(actionTexture);
-    Label keyLabel = new Label(keyText, skin, "large");
-
-    tutorialTable = new Table();
-    tutorialTable.add(actionImage).size(32, 32).padBottom(10f).row();
-    tutorialTable.add(keyLabel);
-
-    stage.addActor(tutorialTable);
+    skin = new Skin(Gdx.files.internal("commodore64/skin/uiskin.json"));
+    font = skin.getFont("commodore-64");
+    font.setUseIntegerPositions(false);
+    super.create();
   }
 
   @Override
   protected void draw(SpriteBatch batch) {
-    Vector3 screenPos = camera.getCamera().project(new Vector3(entity.getCenterPosition(), 0));
-    tutorialTable.setPosition(screenPos.x - tutorialTable.getWidth() / 2, screenPos.y - tutorialTable.getHeight() / 2);
+    super.draw(batch);
+    if (font == null) return;
+    Vector2 scale = entity.getScale();
+    Vector2 pos = entity.getPosition();
+
+    font.getData().setScale(scale.x / 40);
+
+    float textX = pos.x - scale.x / 2;
+    float textY = pos.y - 0.2f;
+
+    font.draw(batch, keyText, textX, textY, scale.x * 2, Align.center, false);
   }
 
   @Override
   public void dispose() {
     super.dispose();
-    if (tutorialTable != null) {
-      tutorialTable.remove();
-    }
-  }
-
-  @Override
-  public int getLayer() {
-    return Integer.MIN_VALUE;
   }
 }
