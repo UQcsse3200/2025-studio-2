47866c5d8c19266fc7e4bcf5bc5bc779d3ea164d
Jacob Marriott <jacobemarriott@icloud.com>
2025-10-08 15:29:02 +1000

    uncommenting

--- source/core/src/main/com/csse3200/game/screens/MainGameScreen.java
+++ source/core/src/main/com/csse3200/game/screens/MainGameScreen.java
@@ -106,9 +106,9 @@ public class MainGameScreen extends ScreenAdapter {
 
 
     //gameArea = new SprintOneGameArea(terrainFactory);
-//    gameArea = new LevelOneGameArea(terrainFactory);
+    gameArea = new LevelOneGameArea(terrainFactory);
     //gameArea = new LevelTwoGameArea(terrainFactory);
-    gameArea = new TutorialTwoGameArea(terrainFactory);
+//    gameArea = new TutorialTwoGameArea(terrainFactory);
 
     gameArea.create();
 
