75d342bb7e63274bdc5092895535b3321d5aa2ae
Bruce Zou <bruce.zou@student.uq.edu.au>
2025-10-08 10:00:16 +1000

    Removed commented code in StatisticsDisplay

--- source/core/src/main/com/csse3200/game/components/statisticspage/StatisticsDisplay.java
+++ source/core/src/main/com/csse3200/game/components/statisticspage/StatisticsDisplay.java
@@ -64,26 +64,6 @@ public class StatisticsDisplay extends UIComponent {
         topTable.add(title).expandX().center().padTop(20f);
         stage.addActor(topTable);
 
-//        Label playtimeLabel =
-//                new Label("Total Playtime: " + StatsTracker.getPlaytimeMinutes() + " mins",
-//                skin);
-//        Label upgradesLabel =
-//                new Label("Upgrades picked up: " + StatsTracker.getUpgradesCollected(), skin);
-//        Label levelCompletedLabel =
-//                new Label("Levels completed: " + StatsTracker.getLevelsCompleted(), skin);
-//        Label deathCounterLabel =
-//                new Label("Deaths: " + StatsTracker.getDeathCount(),
-//                skin);
-//        Label achievementCounterLabel =
-//                new Label("Achievements: " + StatsTracker.getAchievementsUnlocked(),
-//                skin);
-//
-//        table.add(playtimeLabel).pad(10f).padLeft(50f).row();
-//        table.add(upgradesLabel).pad(10f).padLeft(50f).row();
-//        table.add(levelCompletedLabel).pad(10f).padLeft(50f).row();
-//        table.add(deathCounterLabel).pad(10f).padLeft(50f).row();
-//        table.add(achievementCounterLabel).pad(10f).padLeft(50f).row();
-
         Label playtimeLabel = new Label("Total Playtime:", skin);
         Label playtimeValue = new Label(StatsTracker.getPlaytimeMinutes() + " mins", skin);
 
