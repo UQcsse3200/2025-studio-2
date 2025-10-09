c16762e0b23be69a3cc430040540164e7bf61ef4
49745962 <sanyamsingh.brar@student.uq.edu.au>
2025-10-09 15:55:13 +1000

    slight improvements to the leaderboard component

--- source/core/src/main/com/csse3200/game/components/LeaderboardComponent.java
+++ source/core/src/main/com/csse3200/game/components/LeaderboardComponent.java
@@ -3,96 +3,42 @@ package com.csse3200.game.components;
 import com.badlogic.gdx.Gdx;
 import com.csse3200.game.utils.GsonJsonUtils;
 
-import java.io.*;
 import java.util.HashMap;
 import java.util.stream.Collectors;
 
 
 public class LeaderboardComponent {
-    private final String filePath = "configs/leaderboard.json";
-    private final GsonJsonUtils converter = new GsonJsonUtils();
+  private final String filePath = "configs/leaderboard.json";
+  private final GsonJsonUtils converter = new GsonJsonUtils();
+  private final HashMap<String, Long> leaderboard = new HashMap<>();
 
-    public HashMap<String, Long> readData() {
-        return readData(this.filePath);
+  public HashMap<String, Long> readData() {
+    String file;
+    try {
+      file = Gdx.files.external("CSSE3200Game" + filePath).readString();
+    } catch (Exception e) {
+      file = Gdx.files.internal(filePath).readString();
     }
+    return this.converter.jsonToHashMap(file);
+  }
 
-    public HashMap<String, Long> readData(String filePath) {
-        String file;
-        try {
-            file = Gdx.files.external(filePath).readString();
-        } catch (Exception e) {
-            file = Gdx.files.internal(filePath).readString();
-        }
-        return this.converter.jsonToHashMap(file);
-    }
+  public void writeData() {
+    Gdx.files.external("CSSE3200Game" + filePath).writeString(
+        "{\n" + leaderboard
+            .entrySet()
+            .stream()
+            .map(entry -> String.format("\t\"%s\": %s}", entry.getKey(), entry.getValue()))
+            .reduce((str, current) -> str + ",\n" + current) + "\n}",
+        false
+    );
+  }
 
-    public void writeData(HashMap<String, Long> map) {
-        writeData(map, this.filePath);
-    }
+  public void updateLeaderboard(String name, long score, boolean force) {
+    leaderboard.compute(name, (k, v) -> ((force || v == null) ? score : Math.max(v, score)));
+    writeData();
+  }
 
-    public void writeData(HashMap<String, Long> map, String filePath) {
-      Gdx.files.external(filePath).writeString(
-          "{\n" + String.join(map
-              .entrySet()
-              .stream()
-              .map(entry -> String.format("\t\"%s\": %s}", entry.getKey(), entry.getValue()))
-              .collect(Collectors.joining()), ",\n") + "\n}",
-          false
-      );
-    }
-
-
-    public void updateLeaderboard(String name, long score, boolean force) {
-        HashMap<String, Long> leaderboard = readData();
-        Long time = leaderboard.get(name);
-
-        if (force) {
-            leaderboard.put(name, score);
-        } else if (time == null || time > score) {
-            leaderboard.put(name, score);
-        }
-
-        writeData(leaderboard);
-    }
-
-    public void updateLeaderboard(String name, long score) {
-        HashMap<String, Long> leaderboard = readData();
-        Long time = leaderboard.get(name);
-
-        System.out.println(time);
-        System.out.println(score);
-
-        if (time == null || time > score) {
-            leaderboard.put(name, score);
-        }
-
-        writeData(leaderboard);
-    }
-
-    public void updateLeaderboard(String name, long score, boolean force, String filePath) {
-        HashMap<String, Long> leaderboard = readData(filePath);
-        Long time = leaderboard.get(name);
-
-        if (force) {
-            leaderboard.put(name, score);
-        } else if (time == null || time > score) {
-            leaderboard.put(name, score);
-        }
-
-        writeData(leaderboard, filePath);
-    }
-
-    public void updateLeaderboard(String name, long score, String filePath) {
-        HashMap<String, Long> leaderboard = readData(filePath);
-        Long time = leaderboard.get(name);
-
-        System.out.println(time);
-        System.out.println(score);
-
-        if (time == null || time > score) {
-            leaderboard.put(name, score);
-        }
-
-        writeData(leaderboard, filePath);
-    }
+  public void updateLeaderboard(String name, long score) {
+    updateLeaderboard(name, score, false);
+  }
 }
