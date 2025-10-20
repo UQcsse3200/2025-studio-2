cdd9f90f75afa58c2cf6fa83df0be939e0dc3544
49745962 <sanyamsingh.brar@student.uq.edu.au>
2025-10-20 13:59:24 +1000

    feat: #253 enabled and fixed SelfDestructComponentTest

--- source/core/src/test/com/csse3200/game/components/SelfDestructComponentTest.java
+++ source/core/src/test/com/csse3200/game/components/SelfDestructComponentTest.java
@@ -5,21 +5,28 @@ import com.badlogic.gdx.math.Vector2;
 import com.csse3200.game.components.npc.DroneAnimationController;
 import com.csse3200.game.entities.Entity;
 import com.csse3200.game.events.EventHandler;
-import com.csse3200.game.physics.components.PhysicsComponent;
+import com.csse3200.game.extensions.GameExtension;
 import com.csse3200.game.rendering.AnimationRenderComponent;
 import com.csse3200.game.services.ResourceService;
 import com.csse3200.game.services.ServiceLocator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.mockito.Mockito.*;
 
+@ExtendWith(GameExtension.class)
+@ExtendWith(MockitoExtension.class)
 class SelfDestructComponentTest {
     private Entity player;
     private SelfDestructComponent selfDestruct;
     private AnimationRenderComponent animator;
     private Sound sound;
     private CombatStatsComponent playerStats;
+    @Mock
+    ResourceService resourceService;
 
     @BeforeEach
     void setUp() {
@@ -29,7 +36,6 @@ class SelfDestructComponentTest {
         player = mock(Entity.class);
 
         animator = mock(AnimationRenderComponent.class);
-        PhysicsComponent physics = mock(PhysicsComponent.class);
         sound = mock(Sound.class);
         playerStats = mock(CombatStatsComponent.class);
 
@@ -37,17 +43,10 @@ class SelfDestructComponentTest {
         when(drone.getEvents()).thenReturn(events);
 
         when(drone.getComponent(AnimationRenderComponent.class)).thenReturn(animator);
-        when(drone.getComponent(PhysicsComponent.class)).thenReturn(physics);
-        when(player.getComponent(CombatStatsComponent.class)).thenReturn(playerStats);
 
-        // Default positions
         when(drone.getCenterPosition()).thenReturn(new Vector2(0, 0));
         when(player.getCenterPosition()).thenReturn(new Vector2(10, 10));
 
-        // Resource service mock
-        ResourceService resourceService = mock(ResourceService.class);
-        when(resourceService.getAsset("sounds/explosion.mp3", Sound.class)).thenReturn(sound);
-        when(sound.play(anyFloat())).thenReturn(1L);
         ServiceLocator.registerResourceService(resourceService);
 
         DroneAnimationController controller = new DroneAnimationController();
@@ -61,6 +60,10 @@ class SelfDestructComponentTest {
 
     @Test
     void testExplodesWhenTouchingPlayer() {
+        when(resourceService.getAsset(anyString(), eq(Sound.class))).thenReturn(sound);
+        when(sound.play(anyFloat())).thenReturn(1L);
+        when(player.getComponent(CombatStatsComponent.class)).thenReturn(playerStats);
+
         // Within collision radius
         when(player.getCenterPosition()).thenReturn(new Vector2(0.5f, 0.5f));
         when(playerStats.getHealth()).thenReturn(5);
@@ -74,6 +77,10 @@ class SelfDestructComponentTest {
 
     @Test
     void testExplodesWhenTooFarFromPlayer() {
+        when(resourceService.getAsset(anyString(), eq(Sound.class))).thenReturn(sound);
+        when(sound.play(anyFloat())).thenReturn(1L);
+        when(player.getComponent(CombatStatsComponent.class)).thenReturn(playerStats);
+
         when(player.getCenterPosition()).thenReturn(new Vector2(100, 100));
         when(playerStats.getHealth()).thenReturn(10);
 
@@ -90,21 +97,25 @@ class SelfDestructComponentTest {
 
         selfDestruct.update();
 
-        verify(animator, never()).startAnimation("bomb_effect");
-        verify(sound, never()).play(anyFloat());
-        verify(playerStats, never()).setHealth(anyInt());
+        verifyNoInteractions(animator);
+        verifyNoInteractions(sound);
+        verifyNoInteractions(playerStats);
     }
 
     @Test
     void testExplosionOnlyHappensOnce() {
+        when(resourceService.getAsset(anyString(), eq(Sound.class))).thenReturn(sound);
+        when(sound.play(anyFloat())).thenReturn(1L);
+        when(player.getComponent(CombatStatsComponent.class)).thenReturn(playerStats);
+
         when(player.getCenterPosition()).thenReturn(new Vector2(0, 0));
         when(playerStats.getHealth()).thenReturn(10);
 
         selfDestruct.update();
         selfDestruct.update();
 
-        verify(animator, times(1)).startAnimation("bomb_effect");
-        verify(sound, times(1)).play(anyFloat());
-        verify(playerStats, times(1)).setHealth(8);
+        verify(animator).startAnimation("bomb_effect");
+        verify(sound).play(anyFloat());
+        verify(playerStats).setHealth(8);
     }
 }
