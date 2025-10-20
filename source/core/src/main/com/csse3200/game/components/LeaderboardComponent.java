f2c39e733ba014ec80d5eaef1ef05ff8eb78a241
49745962 <sanyamsingh.brar@student.uq.edu.au>
2025-10-20 10:10:35 +1000

    removed a useless function

--- source/core/src/main/com/csse3200/game/components/LeaderboardComponent.java
+++ source/core/src/main/com/csse3200/game/components/LeaderboardComponent.java
@@ -9,11 +9,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class LeaderboardComponent {
-  private final Map<String, Long> leaderboard;
+  private Map<String, Long> leaderboard = new HashMap<>();
   private static final LeaderboardComponent instance = new LeaderboardComponent();
 
   private LeaderboardComponent() {
-    leaderboard = readData();
+    try {
+      leaderboard = new Gson().fromJson(getFile().readString(), new TypeToken<HashMap<String, Long>>() {}.getType());
+    } catch (Exception e) { /* Ignored */ }
   }
 
   public static LeaderboardComponent getInstance() {
@@ -22,14 +24,6 @@ public class LeaderboardComponent {
 
   private FileHandle getFile() {
     return Gdx.files.external("CSSE3200Game/leaderboard.json");
-  }
-
-  private Map<String, Long> readData() {
-    try {
-      return  new Gson().fromJson(getFile().readString(), new TypeToken<HashMap<String, Long>>() {}.getType());
-    } catch (Exception e) {
-      return new HashMap<>();
-    }
   }
 
   public Map<String, Long> getData() {
