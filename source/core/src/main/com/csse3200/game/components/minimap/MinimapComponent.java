cf7bc55182f96977a1e5c61de814580b6cbdebbb
Tristyn Hunt <tristyn.hunt@student.uq.edu.au>
2025-10-21 00:42:17 +1000

    refactored minimap comp to scale markers based off entity scale

--- source/core/src/main/com/csse3200/game/components/minimap/MinimapComponent.java
+++ source/core/src/main/com/csse3200/game/components/minimap/MinimapComponent.java
@@ -2,6 +2,7 @@ package com.csse3200.game.components.minimap;
 
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.csse3200.game.components.Component;
 import com.csse3200.game.services.ServiceLocator;
@@ -10,57 +11,36 @@ import com.csse3200.game.services.ServiceLocator;
  * A component to be added to entities that should be tracked on the minimap.
  */
 public class MinimapComponent extends Component {
-  private final Image marker;
+  private final String markerAsset;
+  private Image marker;
 
-  /**
-   * @param marker The image to use for the entity's marker.
-   */
-  public MinimapComponent(Image marker) {
-    this.marker = marker;
-  }
+  private static final float MARKER_SCALE = 1.56f;
+
 
   /**
    * @param markerAsset The path to the texture for this entity's marker.
    */
   public MinimapComponent(String markerAsset) {
-    this(loadImageWithDefaultSize(markerAsset));
+    this.markerAsset = markerAsset;
   }
 
-  private static Image loadImageWithDefaultSize(String markerAsset) {
-    Image marker = new Image(ServiceLocator.getResourceService().getAsset(markerAsset, Texture.class));
-      switch (markerAsset) {
-          case "images/door_open-map.png" -> marker.setSize(20f, 40f);
-          case "images/platform-map.png" -> marker.setSize(30f, 8f);
-          case "images/platform-long-map.png" -> marker.setSize(60f, 8f);
-          case "images/platform-short-map.png" -> marker.setSize(15f, 8f);
-          case "images/flying_bat_map.png" -> marker.setSize(20f, 20f);
-          case "images/drone-map.png" -> marker.setSize(20f, 10f);
-          case "images/ladder-map.png" -> marker.setSize(13f, 13f);
-          case "images/floor-map-1.png" -> marker.setSize(250f, 208f);
-          case "images/floor-map-2.png" -> marker.setSize(450f, 208f);
-          case "images/floor-map-3.png" -> marker.setSize(200f, 208f);
-          case "images/floor-map-4.png" -> marker.setSize(90f, 35f);
-          case "images/floor-map-5.png" -> marker.setSize(667f, 35f);
-          case "images/floor-map-6.png" -> marker.setSize(750f, 35f);
-          case "images/gate-floor-map.png" -> marker.setSize(100f, 12f);
-          case "images/puzzle-floor-map.png" -> marker.setSize(260f, 35f);
-          case "images/wall-map-1.png" -> marker.setSize(15f, 79f);
-          case "images/wall-map-2.png" -> marker.setSize(45f, 132f);
-          case "images/wall-map-3.png" -> marker.setSize(25f, 130f);
-          case "images/wall-map-4.png" -> marker.setSize(35f, 105f);
-          case "images/wall-map-5.png" -> marker.setSize(35f, 141f);
-          case "images/wall-map-6.png" -> marker.setSize(35f, 115f);
-          case "images/wall-map-7.png" -> marker.setSize(35f, 87f);
-          case "images/wall-map-8.png" -> marker.setSize(35f, 61f);
-          case "images/wall-map-9.png" -> marker.setSize(35f, 33f);
-          default -> marker.setSize(15f, 20f); // Default marker size
-      }
+  private void rescaleMarker() {
+    if (marker == null) return;
 
-    return marker;
+    Vector2 worldSize = entity.getScale().cpy();
+    float pxPerWUX = ServiceLocator.getMinimapService().getPixelsPerWorldUnitX();
+    float pxPerWUY = ServiceLocator.getMinimapService().getPixelsPerWorldUnitY();
+
+    float targetPxW = worldSize.x * pxPerWUX * MARKER_SCALE;
+    float targetPxH = worldSize.y * pxPerWUY * MARKER_SCALE;
+
+    marker.setSize(targetPxW, targetPxH);
   }
 
   @Override
   public void create() {
+    marker = new Image(ServiceLocator.getResourceService().getAsset(markerAsset, Texture.class));
+    rescaleMarker();
     ServiceLocator.getMinimapService().trackEntity(entity, marker);
   }
 
