7f2af008555cbec50228855dfbb06673ababc69b
Tristyn Hunt <tristyn.hunt@student.uq.edu.au>
2025-10-21 22:20:30 +1000

    fixed minimap to scale with screen aspect ratio

--- source/core/src/main/com/csse3200/game/components/minimap/MinimapComponent.java
+++ source/core/src/main/com/csse3200/game/components/minimap/MinimapComponent.java
@@ -1,5 +1,6 @@
 package com.csse3200.game.components.minimap;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.GridPoint2;
@@ -18,8 +19,11 @@ public class MinimapComponent extends Component {
   private final Vector2 MARKER_SCALE = new Vector2();
   private float scaleX = 1f;
   private float scaleY = 1f;
-  private static final float BOUNDS_SCALAR = 0.0222857f; // dont ask...
+  private static final float BOUNDS_SCALAR = 0.01253571f; // dont ask...
   private static final GridPoint2 DEFAULT_BOUNDS = new GridPoint2(100, 100);
+  private GridPoint2 tileBounds;
+
+  private float lastScreenRatio = -1f;
 
 
   /**
@@ -45,15 +49,31 @@ public class MinimapComponent extends Component {
   @Override
   public void create() {
     var screen = ServiceLocator.getMainGameScreen();
-    GridPoint2 tileBounds = screen == null ? DEFAULT_BOUNDS : screen.getGameArea().getMapBounds();
-    MARKER_SCALE.set(tileBounds.x * BOUNDS_SCALAR * scaleX, tileBounds.y * BOUNDS_SCALAR * scaleY);
+    tileBounds = screen == null ? DEFAULT_BOUNDS : screen.getGameArea().getMapBounds();
+    float ratio = (float) Gdx.graphics.getWidth() / Gdx.graphics.getHeight();
+    lastScreenRatio = ratio;
+
+    MARKER_SCALE.set(tileBounds.x * BOUNDS_SCALAR * scaleX * ratio, tileBounds.y * BOUNDS_SCALAR * scaleY * ratio);
+
     Texture asset = ServiceLocator.getResourceService().getAsset(markerAsset, Texture.class);
     if (asset == null) return;
     marker = new Image(asset);
+
     var svs = ServiceLocator.getMinimapService();
     if (svs == null) return;
     svs.trackEntity(entity, marker);
+
     rescaleMarker();
+  }
+
+  @Override
+  public void update() {
+    float ratio = (float) Gdx.graphics.getWidth() / Gdx.graphics.getHeight();
+    if (Math.abs(ratio - lastScreenRatio) > 1e-4) {
+      MARKER_SCALE.set(tileBounds.x * BOUNDS_SCALAR * scaleX * ratio, tileBounds.y * BOUNDS_SCALAR * scaleY * ratio);
+      rescaleMarker();
+      lastScreenRatio = ratio;
+    }
   }
 
   /**
